981de97f234ff7a68f35254f02f6b4b92fb07981
Игорь <gariksyr@gmail.com>
2025-07-06 22:43:24 +0300

    testing batch update

--- src/main/java/its/fuckin/CRUD/dao/PersonDAO.java
+++ src/main/java/its/fuckin/CRUD/dao/PersonDAO.java
@@ -2,6 +2,7 @@ package its.fuckin.CRUD.dao;
 
 import its.fuckin.CRUD.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
@@ -35,5 +36,44 @@ public class PersonDAO {
     }
     public void delete(int id){
         jdbcTemplate.update("DELETE FROM person WHERE id=?", id);
+    }
+
+    /// ////////////тест производительности пакетной вставки
+    public void testMultipleUpdate(){
+        List<Person> people = create1000People();
+        long before = System.currentTimeMillis();
+        for (Person person : people){
+            jdbcTemplate.update("INSERT INTO person VALUES(?, ?, ?, ?)", person.getId(), person.getName(),person.getAge(), person.getEmail());
+        }
+        long after = System.currentTimeMillis();
+        System.out.println(after - before);
+    }
+    public void testBatchUpdate(){
+        List<Person> people = create1000People();
+        long before = System.currentTimeMillis();
+        jdbcTemplate.batchUpdate("INSERT INTO person VALUES(?, ?, ?, ?)", new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                ps.setInt(1,people.get(i).getId());
+                ps.setString(2,people.get(i).getName());
+                ps.setInt(3,people.get(i).getAge());
+                ps.setString(4,people.get(i).getEmail());
+            }
+
+            @Override
+            public int getBatchSize() {
+                return people.size();
+            }
+        });
+        long after = System.currentTimeMillis();
+        System.out.println(after - before);
+    }
+
+    private List<Person> create1000People() {
+        List<Person> people = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            people.add(new Person(i, "Name" + i, 30, "test" + i + "mail.ru"));
+        }
+        return people;
     }
 }
