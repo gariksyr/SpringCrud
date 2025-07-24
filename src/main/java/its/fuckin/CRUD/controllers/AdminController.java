adfe9e439db8aa944751d69fed49d4febb3d8b42
Игорь <gariksyr@gmail.com>
2025-07-24 19:59:25 +0300

    dropdown menu with choosing admin user

---
+++ src/main/java/its/fuckin/CRUD/controllers/AdminController.java
@@ -0,0 +1,30 @@
+package its.fuckin.CRUD.controllers;
+
+import its.fuckin.CRUD.dao.PersonDAO;
+import its.fuckin.CRUD.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+    private PersonDAO personDAO;
+
+    @Autowired
+    public AdminController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+    @GetMapping()
+    public String adminPage(Model model, @ModelAttribute("person") Person person){
+        model.addAttribute("people", personDAO.index());
+        return "people/adminPage";
+    }
+    @PatchMapping("/add")
+    public String makeAdmin(@ModelAttribute("person") Person person){
+        System.out.println(person.getId());
+
+        return "redirect:/people";
+    }
+}
